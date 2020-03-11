346ff2971c46b466968783a62d0d938d97624aea
chenguohong <935859473@qq.com>
2020-03-11 16:06:22 +0800

    添加nginx负载均衡服务

--- skillMain.go
+++ skillMain.go
@@ -18,14 +18,14 @@ func main()  {
 	//2.设置错误模式，在mvc模式下提示错误
 	app.Logger().SetLevel("debug" )
 	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1> Hello iris </h1>")
+		ctx.HTML("<h1> Hello nginx2 </h1>")
 	})
 	//4.设置模板目标
 	app.StaticWeb("/public", "./web/views/home/public")
 	app.StaticWeb("/html", "./web/views/home/htmlProductShow")
 	//6.启动服务
 	app.Run(
-		iris.Addr("0.0.0.0:80"),
+		iris.Addr("0.0.0.0:9081"),
 		iris.WithoutServerError(iris.ErrServerClosed),
 		iris.WithOptimizations,
 	)
